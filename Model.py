8ab17069c7220ca622aa20ac19805c08998b84e4
Prarit Agarwal <agarwalprarit@gmail.com>
2020-05-08 12:17:05 +0100

    Create a Model class for the NN architecture

--- Model.py
+++ Model.py
@@ -35,11 +35,11 @@ class QNetwork(nn.Module):
         torch.manual_seed(seed)
         self.l1 = nn.Linear(in_features = self.state_size, out_features = hidden_size, bias = True)
         # self.batch1 = nn.BatchNorm1d(num_features = 50)
-        self.l2 = nn.Linear(in_features = hidden_size, out_features = hidden_size/2, bias = True)
+        self.l2 = nn.Linear(in_features = hidden_size, out_features = hidden_size//2, bias = True)
         # self.batch2 = nn.BatchNorm1d(num_features = 50)
-        self.l3 = nn.Linear(in_features = hidden_size/2, out_features = hidden_size/4, bias = True)
+        self.l3 = nn.Linear(in_features = hidden_size//2, out_features = hidden_size//4, bias = True)
         # self.batch3 = nn.BatchNorm1d(num_features = 50)
-        self.l4 = nn.Linear(in_features = hidden_size/4, out_features = action_size, bias = True)
+        self.l4 = nn.Linear(in_features = hidden_size//4, out_features = action_size, bias = True)
         self.dropout = nn.Dropout(p = self.drop_prob)
         
     def forward(self, state):
